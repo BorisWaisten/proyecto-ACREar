77eefc96d46f5f9736b54dce387ad943b44f247b
BorisWaisten <104634915+BorisWaisten@users.noreply.github.com>
2025-06-11 22:31:37 -0300

    size changes

--- proyecto-acrear/components/home/maps-section.jsx
+++ proyecto-acrear/components/home/maps-section.jsx
@@ -10,7 +10,7 @@ export default function MapSection({ regional, provincias }) {
 
   return (
     <motion.section
-      className="w-full min-h-[500px] sm:min-h-[600px] md:h-[70vh] lg:h-screen relative overflow-hidden py-10 sm:py-16"
+      className="w-full min-h-[400px] sm:min-h-[600px] md:h-[70vh] relative overflow-hidden py-10 sm:py-0 "
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6, ease: 'easeOut' }}
@@ -26,7 +26,7 @@ export default function MapSection({ regional, provincias }) {
         <div className="grid grid-cols-2 md:grid-cols-2 gap-8">
           {/* Mapa interactivo */}
           <motion.div
-            className="w-full min-h-[250px] sm:h-full md:h-full pl-2 sm:pl-0 rounded-xl overflow-hidden"
+            className="w-[250px] pr-10 scale-[1.6] sm:pr-0 sm:scale-100  sm:w-full min-h-[250px] sm:h-full md:h-full pl-2 sm:pl-0 rounded-xl overflow-hidden"
             initial={{ opacity: 0, scale: 0.9 }}
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
@@ -44,20 +44,20 @@ export default function MapSection({ regional, provincias }) {
               exit={{ opacity: 0, y: -10 }}
               transition={{ duration: 0.3 }}
             >
-              <div className="flex flex-col w-full  text-[var(--color-primary)] justify-center ">
+              <div className="flex flex-col w-full h-full text-[var(--color-primary)] justify-center ">
                 <p className="text-lg md:pb-6 md:text-2xl font-bold text-center">
                   {provincia.name}
                 </p>
                 {provincia.activities.map((act, idx) => (
-                  <div key={idx} className="flex items-center gap-4">
+                  <div key={idx} className="flex sm:w-[100px] sm:mb-4 md:w-[450px] sm:mx-auto items-center gap-4">
                     <Image
                       src={provincia.icons[idx]}
                       alt={act}
                       width={60}
                       height={60}
-                      className="mb-2 w-10 h-10"
+                      className="mb-2 w-8 h-8 sm:w-10 sm:h-10 md:w-16 md:h-16"
                     />
-                    <p className="text-xs sm:text-sm text-balance">{act}</p>
+                    <p className="text-xs md:w-full sm:text-sm text-balance sm:text-center md:text-lg">{act}</p>
                   </div>
                 ))}
               </div>
